b86b1a06c174f5ca1f56a936315e978db5143eea
Girmay Addisu <emmanda2050@gmail.com>
2024-11-20 08:58:05 +0300

    serving static files

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 // 1. MIDDLEWARE
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use(express.json());  // middleware
 app.use((req, res, next) => {
