7e93e90608468905835a4c8abaf9c10f331b5035
Girmay Addisu <emmanda2050@gmail.com>
2024-11-22 11:30:28 +0300

    aliasing

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,5 +1,13 @@
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-price,-ratingsAverage';
+  req.query.fields = 'name,ratingsAverage,summary,difficulty'
+  next();
+}
+
+
 exports.getAllTours = async (req, res) => {
   try {
 
