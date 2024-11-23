2ca1f780bbf5971a950ce09404ee19d2530497d6
Girmay Addisu <emmanda2050@gmail.com>
2024-11-23 09:59:16 +0300

    handling unhandled routes

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -14,11 +14,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello From the middleware 👌');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -26,4 +21,10 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `This ${req.originalUrl} url is not found!`
+  });
+});
 module.exports = app;
