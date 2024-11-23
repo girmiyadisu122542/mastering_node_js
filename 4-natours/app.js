11b4d4c959558eb2951b475dbe5a916babac84a6
Girmay Addisu <emmanda2050@gmail.com>
2024-11-23 11:23:45 +0300

    implementing global error handling middleware

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -22,9 +22,27 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `This ${req.originalUrl} url is not found!`
-  });
+
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `This ${req.originalUrl} url is not found!`
+  // });
+
+  const err = new Error(`This ${req.originalUrl} url is not found!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+
+
 });
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error'
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+})
 module.exports = app;
