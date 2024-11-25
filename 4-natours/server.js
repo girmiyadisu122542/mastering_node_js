9ac19981898661797236ebc78f4cde2601310779
Girmay Addisu <emmanda2050@gmail.com>
2024-11-25 15:25:18 +0300

    Erros outside express uncaought exception errors

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -2,6 +2,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log("UNCAUGHT EXCEPTION: 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -12,7 +19,6 @@ mongoose
   .connect(DB)
   .then(() => console.log('Database connected successfully!'));
 
-
 const port = 3000;
 const server = app.listen(port, () => {
   console.log(`App Listening at port ${port}...`);
@@ -20,7 +26,7 @@ const server = app.listen(port, () => {
 
 process.on('unhandledRejection', err => {
   console.log("UNHANDLED REJECTION: 💥 shutting down...");
-  console.log(err);
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   })
