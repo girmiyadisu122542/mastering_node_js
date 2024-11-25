ae92eeb5e61ac482c70ac0e6c4f695e966c6b2b5
Girmay Addisu <emmanda2050@gmail.com>
2024-11-25 14:03:07 +0300

    Handling invalid databse IDs

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,4 +1,9 @@
+const AppError = require('../utils/appError');
 
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+}
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -40,7 +45,13 @@ module.exports = (err, req, res, next) => {
         sendErrorDev(err, res);
     }
     else if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res);
+
+        let error = { ...err };
+
+        if (err.name === 'CastError') {
+            error = handleCastErrorDB(err);
+        }
+        sendErrorProd(error, res);
     }
 
 
