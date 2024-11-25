20fc54864598968eb9063f4d7450250f2d8f1bdc
Girmay Addisu <emmanda2050@gmail.com>
2024-11-25 11:13:07 +0300

    sending errors in development and production

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,11 +1,47 @@
 
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+}
+
+const sendErrorProd = (err, res) => {
+
+    //Operational: trusted error send to the client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    }
+    //Programming or unknow error:  errors not to be seen for the client only for developers
+    else {
+        //1) log error
+        console.error("ERROR 💥");
+        // 2) send generic message
+        res.status(500).json({
+            status: 'error',
+            message: "Something went wrong",
+        });
+    }
+
+}
+
 module.exports = (err, req, res, next) => {
     console.log(err.stack);
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error'
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    }
+    else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res);
+    }
+
+
 }
