8a94b6c2f6f827dbec3972ebd65fde04d75a4a54
Girmay Addisu <emmanda2050@gmail.com>
2024-11-26 10:40:59 +0300

    Managing passwords

--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -17,14 +18,32 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Enter your password'],
-        minlength: 8
+        minlength: 8,
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your password']
+        required: [true, 'Please confirm your password'],
+        //This only works on CREATE and SAVE
+        validate: {
+            validator: function (el) {
+                return el === this.password
+            },
+            message: 'Passwords are not the same'
+        }
     }
 });
 
+userSchema.pre('save', async function (next) {
+    //Only run this function if password was actually modefied   
+    if (!this.isModified('password')) return next();
+
+    //Hashing password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //Delting the passwordConfirm field
+    this.passwordConfirm = undefined;
+
+});
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
